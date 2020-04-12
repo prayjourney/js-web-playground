a9836772b6ea158cd95a66d02da85daa3a16b5d9
zuiguangyin <2247359268@qq.com>
2020-04-12 17:34:53 +0800

    js和json的转换，json对象和json数组，前端和后端

--- js/js&json.js
+++ js/js&json.js
@@ -14,3 +14,9 @@ console.log(str)
 var user02 = JSON.parse(str)
 
 console.log(user02)
+
+// note:
+// json有对象和数组两种形式，对象是{ name: 'zgy', age: 23, gender: 'male', location: '成都' }
+// json数组的话是[{name: 'hello', age: 22},{name: 'zgy1', age: 23},{name: '王麻子', age: 19}]
+// json和javascript转换基本使用JSON.stringify和JSON.parse即可，这是在前端，
+// 在后端的话，主流使用jackson和fastjson即可。
