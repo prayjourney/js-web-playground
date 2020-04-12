5ea390e268b5fb1b95879b6d27345619b22ae7f5
zuiguangyin <2247359268@qq.com>
2020-04-12 12:11:34 +0800

    基本的语法和数据格式

---
+++ js/basicSyntax.js
@@ -0,0 +1,19 @@
+var s = 100; // 定义变量
+var name; // 没有赋值，默认的值是undifined
+
+"hello world!" // 字符串字面量
+1234 ; // 数字字面量
+
+var Location ="成都"
+var location = "chengdu" // 大小写敏感
+
+/*空值, null, undefined的比较*/
+var person;           // 值是 undefined，类型是 undefined
+person = undefined;
+var car = "";         // 值是 ""，类型是 "string"
+var name = null;           // 值是 null，但是类型仍然是对象
+
+var length = 7;                             // 数字
+var lastName = "Gates";                      // 字符串
+var cars = ["Porsche", "Volvo", "BMW"];         // 数组
+var x = {firstName:"Bill", lastName:"Gates"};    // 对象
